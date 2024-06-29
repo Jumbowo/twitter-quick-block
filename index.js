f9886a388b398eab4ffcbc63371feb6ba862ce0d
Bryan Yang <bryan.yang255@gmail.com>
2024-06-29 23:09:58 +1000

    add native-style hover effects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 // Icon taken from Twitter
-const blockIcon = '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi"><g><path d="M12 3.75c-4.55 0-8.25 3.69-8.25 8.25 0 1.92.66 3.68 1.75 5.08L17.09 5.5C15.68 4.4 13.92 3.75 12 3.75zm6.5 3.17L6.92 18.5c1.4 1.1 3.16 1.75 5.08 1.75 4.56 0 8.25-3.69 8.25-8.25 0-1.92-.65-3.68-1.75-5.08zM1.75 12C1.75 6.34 6.34 1.75 12 1.75S22.25 6.34 22.25 12 17.66 22.25 12 22.25 1.75 17.66 1.75 12z"></path></g></svg>'
+const blockIcon = '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi"><g><path d="M12 3.75c-4.55 0-8.25 3.69-8.25 8.25 0 1.92.66 3.68 1.75 5.08L17.09 5.5C15.68 4.4 13.92 3.75 12 3.75zm6.5 3.17L6.92 18.5c1.4 1.1 3.16 1.75 5.08 1.75 4.56 0 8.25-3.69 8.25-8.25 0-1.92-.65-3.68-1.75-5.08zM1.75 12C1.75 6.34 6.34 1.75 12 1.75S22.25 6.34 22.25 12 17.66 22.25 12 22.25 1.75 17.66 1.75 12z"></path></g></svg>';
 
 function addBlockButtons() {
   const tweets = [...document.getElementsByTagName("article")]
@@ -21,6 +22,10 @@ function addBlockButtons() {
     blockIconButton.setAttribute("data-testid", "block");
 
     blockIconButton.addEventListener("click", (event) => block(event.target));
+
+    // Add hover animations back manually
+    const hoverDiv = blockIconDiv.querySelector("div:has(+ svg)");
+    hoverDiv.classList.add("tqbHoverDiv");
 
     // Mark block button added
     tweet.setAttribute("twitterQuickBlock", "true");
