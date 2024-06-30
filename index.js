a474433c2156ba3c947cf2c19700f4b530ad520e
Bryan Yang <bryan.yang255@gmail.com>
2024-06-30 20:50:17 +1000

    clean up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Icon taken from Twitter
 const blockIcon = '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi"><g><path d="M12 3.75c-4.55 0-8.25 3.69-8.25 8.25 0 1.92.66 3.68 1.75 5.08L17.09 5.5C15.68 4.4 13.92 3.75 12 3.75zm6.5 3.17L6.92 18.5c1.4 1.1 3.16 1.75 5.08 1.75 4.56 0 8.25-3.69 8.25-8.25 0-1.92-.65-3.68-1.75-5.08zM1.75 12C1.75 6.34 6.34 1.75 12 1.75S22.25 6.34 22.25 12 17.66 22.25 12 22.25 1.75 17.66 1.75 12z"></path></g></svg>';
 
@@ -20,6 +19,8 @@ function addBlockButtons() {
     blockIconButton = blockIconDiv.querySelector("button");
     blockIconButton.setAttribute("aria-label", "Block");
     blockIconButton.setAttribute("data-testid", "block");
+    blockIconButton.removeAttribute("aria-expanded");
+    blockIconButton.removeAttribute("aria-haspopup");
 
     blockIconButton.addEventListener("click", (event) => block(event.target));
 
@@ -42,8 +43,6 @@ async function block(target) {
   // Modify a Twitter div to prevent the menu from briefly flashing
   const layers = document.querySelector("#layers");
   layers.classList.add("tqbHideOverlay");
-
-  // TODO: Fix the weird side content background color flash
 
   await new Promise((resolve) => setTimeout(resolve, 5));
   document.querySelector("div[role=\"menuitem\"][data-testid=\"block\"]").click();
