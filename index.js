4345b980c3eb996c99613d4a2be88e4302fa1aef
Bryan Yang <bryan.yang255@gmail.com>
2024-07-01 16:00:55 +1000

    add basic error handling

--- index.js
+++ index.js
@@ -5,77 +5,101 @@ let currentUser = null;
 
 function addBlockButtons() {
   if (currentUser === null) currentUser = getCurrentUser();
+  if (currentUser === null) return;
 
   const tweets = [...document.getElementsByTagName("article")]
     .filter((tweet) => {
-      return tweet.getAttribute("data-testid") === "tweet" && tweet.getAttribute("twitterQuickBlock") !== "true";
+      return (tweet.getAttribute("data-testid") === "tweet") && (tweet.getAttribute("twitterQuickBlock") !== "true");
     })
     // Don't add block button to own tweets
     .filter((tweet) => {
-      return (
-        tweet.querySelector("div[data-testid=\"User-Name\"] a[role=\"link\"]").getAttribute("href") !== currentUser
-      );
+      return tweet.querySelector("div[data-testid=\"User-Name\"] a[role=\"link\"]").getAttribute("href") !== currentUser;
     });
 
   for (const tweet of tweets) {
-    const bottomBar = tweet.querySelector("div[role=\"group\"]:last-child");
+    try {
+      const bottomBar = tweet.querySelector("div[role=\"group\"]:last-child");
 
-    // Copy styling of rightmost button
-    const blockIconDiv = bottomBar.children[bottomBar.children.length - 1].cloneNode(true);
-    blockIconDiv.querySelector("svg").outerHTML = blockIcon;
+      // Copy styling of rightmost button
+      const blockIconDiv = bottomBar.children[bottomBar.children.length - 1].cloneNode(true);
+      blockIconDiv.querySelector("svg").outerHTML = blockIcon;
 
-    // Class for extension custom styles
-    blockIconDiv.className += " tqbBlockButton";
+      // Class for extension custom styles
+      blockIconDiv.className += " tqbBlockButton";
 
-    // Fix attributes
-    blockIconButton = blockIconDiv.querySelector("button");
-    blockIconButton.setAttribute("aria-label", "Block");
-    blockIconButton.setAttribute("data-testid", "block");
-    blockIconButton.removeAttribute("aria-expanded");
-    blockIconButton.removeAttribute("aria-haspopup");
+      // Fix attributes
+      blockIconButton = blockIconDiv.querySelector("button");
+      blockIconButton.setAttribute("aria-label", "Block");
+      blockIconButton.setAttribute("data-testid", "block");
+      blockIconButton.removeAttribute("aria-expanded");
+      blockIconButton.removeAttribute("aria-haspopup");
 
-    blockIconButton.addEventListener("click", (event) => block(event.target));
+      blockIconButton.addEventListener("click", (event) => block(event.target));
 
-    // Add hover animations back manually
-    const hoverDiv = blockIconDiv.querySelector("div:has(+ svg)");
-    hoverDiv.classList.add("tqbHoverDiv");
+      // Add hover animations back manually
+      const hoverDiv = blockIconDiv.querySelector("div:has(+ svg)");
+      hoverDiv.classList.add("tqbHoverDiv");
 
-    // Mark block button added
-    tweet.setAttribute("twitterQuickBlock", "true");
+      // Mark block button added
+      tweet.setAttribute("twitterQuickBlock", "true");
 
-    bottomBar.insertBefore(blockIconDiv, bottomBar.children[4]);
+      bottomBar.insertBefore(blockIconDiv, bottomBar.children[4]);
+    } catch (error) {
+      console.error("TQB error: " + error);
+    }
   }
 }
 
 async function block(target) {
-  const tweet = target.closest("article[twitterQuickBlock=true]");
+  try {
+    const tweet = target.closest("article[twitterQuickBlock=true]");
 
-  // Use the native block button
-  tweet.querySelector("button[data-testid=\"caret\"").click();
-  
-  // Temporarily modify a Twitter div to prevent the menu from briefly flashing
-  // TODO: Fix weird flash in media viewing mode
-  const layers = document.querySelector("#layers");
-  layers.classList.add("tqbHideOverlay");
+    // Use the native block button
+    tweet.querySelector("button[data-testid=\"caret\"").click();
 
-  // TODO: Add loop/check in case of failure
-  await new Promise((resolve) => setTimeout(resolve, 5));
-  document.querySelector("div[role=\"menuitem\"][data-testid=\"block\"]").click();
-  await new Promise((resolve) => setTimeout(resolve, 5));
-  document.querySelector("button[data-testid=\"confirmationSheetConfirm\"]").click();
+    // Temporarily modify a Twitter div to prevent the menu from briefly flashing
+    // TODO: Fix weird flash in media viewing mode
+    const layers = document.querySelector("#layers");
+    layers.classList.add("tqbHideOverlay");
 
-  layers.classList.remove("tqbHideOverlay");
+    await new Promise((resolve) => setTimeout(resolve, 5));
+    document.querySelector("div[role=\"menuitem\"][data-testid=\"block\"]").click();
+    await new Promise((resolve) => setTimeout(resolve, 5));
+    document.querySelector("button[data-testid=\"confirmationSheetConfirm\"]").click();
+
+    layers.classList.remove("tqbHideOverlay");
+  } catch (error) {
+    console.error("Error blocking user.");
+    console.error("TQB error: " + error);
+  }
 }
 
-function getCurrentUser() {  
+function getCurrentUser() {
   // Use the profile button's link to get the current user's tag
   const currentUser = document.querySelector("a[data-testid=\"AppTabBar_Profile_Link\"]").getAttribute("href");
   return currentUser;
 }
 
-console.log("Loading Twitter Quick Block");
+function terminate() {
+  console.error("Twitter Quick Block encountered an error and is terminating. This may be due to a Twitter update."
+    + " Please check https://github.com/Jumbowo/twitter-quick-block/issues for updates.");
+  observer.disconnect(); 
+}
 
-const targetNode = document.querySelector("body");
-const observerConfig = { attributes: false, childList: true, subtree: true };
-const observer = new MutationObserver(addBlockButtons);
-observer.observe(targetNode, observerConfig);
+// Checks to see if extension will work properly
+function checkCompatibility() {
+
+}
+
+try {
+  console.log("Loading Twitter Quick Block");
+  checkCompatibility();
+  const targetNode = document.querySelector("body");
+  const observerConfig = { attributes: false, childList: true, subtree: true };
+  const observer = new MutationObserver(addBlockButtons);
+  observer.observe(targetNode, observerConfig);
+} catch (error) {
+  console.error("Twitter Quick Block failed to load.");
+  terminate();
+}
+
